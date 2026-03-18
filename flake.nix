ce2834c76ce6abca24ca1a5f73a7a27dd4e9b540
Soyukke <soyukke@users.noreply.github.com>
2026-03-19 06:41:12 +0900

    Initial commit: set up Zig project with Nix flake dev environment

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "terminal-commander - Zig project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+    zig-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, zig-overlay }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ zig-overlay.overlays.default ];
+          };
+          zig = pkgs.zigpkgs."0.15.2";
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              zig
+              pkgs.zls
+            ];
+            shellHook = ''
+              export ZIG_LOCAL_CACHE_DIR="$PWD/.zig-cache"
+              export ZIG_GLOBAL_CACHE_DIR="$HOME/.cache/zig"
+            '';
+          };
+        }
+      );
+    };
+}
